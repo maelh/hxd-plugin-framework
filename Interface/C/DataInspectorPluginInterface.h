b268312f538521606b8843ae706ef81a915c8054
Maël <42317414+maelh@users.noreply.github.com>
2020-02-27 12:08:11 +0100

    - use spaces consistently - put GetDataTypeConverterClassIDs to the top like in Delphi source

--- Interface/C/DataInspectorPluginInterface.h
+++ Interface/C/DataInspectorPluginInterface.h
@@ -7,45 +7,47 @@ extern "C" {
 #include "Windows.h"
 #include "DataInspectorShared.h"
 
+BOOL __stdcall GetDataTypeConverterClassIDs(
+    PConverterClassID* ClassIdsOrFactoryFuncs,
+    int* Count);
+
+
 void* __stdcall CreateConverter(
-	TConverterClassID ClassIdOrFactoryFunc,
-	const wchar_t** TypeName,
-	const wchar_t** FriendlyTypeName,
-	TDataTypeWidth* Width,
-	int* MaxTypeSize,
-	TByteOrders* SupportedByteOrders);
+    TConverterClassID ClassIdOrFactoryFunc,
+    const wchar_t** TypeName,
+    const wchar_t** FriendlyTypeName,
+    TDataTypeWidth* Width,
+    int* MaxTypeSize,
+    TByteOrders* SupportedByteOrders);
 
 void __stdcall DestroyConverter(
-	void* ThisPtr);
+    void* ThisPtr);
 
 void __stdcall AssignConverter(
-	void* ThisPtr,
-	void* Source);
+    void* ThisPtr,
+    void* Source);
+
 
 void __stdcall ChangeByteOrder(
-	void* ThisPtr,
-	uint8_t* Bytes,
-	int ByteCount,
-	TByteOrder TargetByteOrder);
+    void* ThisPtr,
+    uint8_t* Bytes,
+    int ByteCount,
+    TByteOrder TargetByteOrder);
 
 TBytesToStrError __stdcall BytesToStr(
-	void* ThisPtr,
-	uint8_t* Bytes,
-	int ByteCount,
-	TIntegerDisplayOption IntegerDisplayOption,
-	int* ConvertedByteCount,
-	const wchar_t** ConvertedStr);
+    void* ThisPtr,
+    uint8_t* Bytes,
+    int ByteCount,
+    TIntegerDisplayOption IntegerDisplayOption,
+    int* ConvertedByteCount,
+    const wchar_t** ConvertedStr);
 
 TStrToBytesError __stdcall StrToBytes(
-	void* ThisPtr,
-	const wchar_t* Str,
-	TIntegerDisplayOption IntegerDisplayOption,
-	uint8_t** ConvertedBytes,
+    void* ThisPtr,
+    const wchar_t* Str,
+    TIntegerDisplayOption IntegerDisplayOption,
+    uint8_t** ConvertedBytes,
     int* ConvertedByteCount);
-
-BOOL __stdcall GetDataTypeConverterClassIDs(
-	PConverterClassID* ClassIdsOrFactoryFuncs,
-	int* Count);
 
 #ifdef __cplusplus  
 }
