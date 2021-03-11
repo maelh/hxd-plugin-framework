86afeb62ea985d2d05b39da14843243f65df2cd9
Maël <42317414+maelh@users.noreply.github.com>
2021-03-11 13:05:07 +0100

    Add more extensive formatting options

--- Interface/Delphi/DataInspectorShared.pas
+++ Interface/Delphi/DataInspectorShared.pas
@@ -9,8 +9,23 @@ type
   TByteOrders = set of TByteOrder;
   TDataTypeWidth = (dtwVariable, dtwFixed);
 
-  TIntegerDisplayOption = (idoDecimal, idoHexadecimalUpperCase,
-    idoHexadecimalLowerCase);
+  TIntegerBase = (ibDecimal, ibHexadecimal);
+  THexBaseIndication = (hbiPascalAndMotorola, hbiC, hbiIntelNoLeadingZero,
+    hbiIntelLeadingZero);
+  TLetterCase = (lcUpperCase, lcLowerCase);
+
+  TFormattingOptions = record
+    IntegerBase: TIntegerBase;
+    HexBaseIndication: THexBaseIndication;
+    HexCasing: TLetterCase;
+    InstructionCasing: TLetterCase;
+    constructor Create(IntegerBase: TIntegerBase;
+      HexBaseIndication: THexBaseIndication; HexCasing: TLetterCase;
+      InstructionCasing: TLetterCase);
+
+    class operator Equal(A, B: TFormattingOptions): Boolean;
+    class operator NotEqual(A, B: TFormattingOptions): Boolean;
+  end;
 
   TBytesToStrError = (btseNone, btseInvalidBytes, btseBytesTooShort);
   TStrToBytesError = (stbeNone, stbeInvalidString, stbeUnderflow, stbeOverflow,
@@ -21,6 +36,31 @@ type
   TConverterClassID = type Pointer;
 
 implementation
+
+{ TFormattingOptions }
+
+constructor TFormattingOptions.Create(IntegerBase: TIntegerBase;
+  HexBaseIndication: THexBaseIndication; HexCasing: TLetterCase;
+  InstructionCasing: TLetterCase);
+begin
+  Self.IntegerBase := IntegerBase;
+  Self.HexBaseIndication := HexBaseIndication;
+  Self.HexCasing := HexCasing;
+  Self.InstructionCasing := InstructionCasing;
+end;
+
+class operator TFormattingOptions.Equal(A, B: TFormattingOptions): Boolean;
+begin
+  Result := (A.IntegerBase = B.IntegerBase) and
+    (A.HexBaseIndication = B.HexBaseIndication) and
+    (A.HexCasing = B.HexCasing) and
+    (A.InstructionCasing = B.InstructionCasing);
+end;
+
+class operator TFormattingOptions.NotEqual(A, B: TFormattingOptions): Boolean;
+begin
+  Result := not (A = B);
+end;
 
 end.
 
